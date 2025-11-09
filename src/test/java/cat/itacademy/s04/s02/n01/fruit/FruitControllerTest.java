a9031a52fd14fbe91d02b34c89b9f189cc6ffc21
Pantalois <loisgonzalezalonso@gmail.com>
2025-11-09 16:58:05 +0100

    feat: added test for FruitController with invalid data

--- src/test/java/cat/itacademy/s04/s02/n01/fruit/FruitControllerTest.java
+++ src/test/java/cat/itacademy/s04/s02/n01/fruit/FruitControllerTest.java
@@ -2,13 +2,10 @@ package cat.itacademy.s04.s02.n01.fruit;
 
 import cat.itacademy.s04.s02.n01.fruit.model.Fruit;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.web.JsonPath;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -34,5 +31,18 @@ public class FruitControllerTest {
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.name").value("Apple"))
                 .andExpect(jsonPath("$.weightInKilos").value(1));
+    }
+
+    @Test
+    void addFruit_whenDataIsInvalid_thenReturn400() throws Exception {
+        Fruit fruit1 = new Fruit("Apple", 0);
+        Fruit fruit2 = new Fruit("", 1);
+        mockMvc.perform(post("/fruits")
+                .contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(fruit1)))
+                .andExpect(status().isBadRequest());
+
+        mockMvc.perform(post("/fruits")
+                .contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(fruit2)))
+                .andExpect(status().isBadRequest());
     }
 }
