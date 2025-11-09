74e3d26f2ba43263efcec104c1da757c2eda101e
Pantalois <loisgonzalezalonso@gmail.com>
2025-11-09 19:14:23 +0100

    feat: Added H2 properties. Working workflow from UserControler to DB

--- src/main/java/cat/itacademy/s04/s02/n01/fruit/repository/FruitRepository.java
+++ src/main/java/cat/itacademy/s04/s02/n01/fruit/repository/FruitRepository.java
@@ -1,7 +1,7 @@
 package cat.itacademy.s04.s02.n01.fruit.repository;
 
 import cat.itacademy.s04.s02.n01.fruit.model.Fruit;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FruitRepository {
-    Fruit save(Fruit fruit);
+public interface FruitRepository extends JpaRepository<Fruit, Long> {
 }
